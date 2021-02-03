75e437748009c1cb25d0dd9866635d8e6128db7b
sloth740 <71675077+sloth740@users.noreply.github.com>
2021-02-02 23:59:22 -0800

    fixed issue with grabbing the input value, need to work on displaying info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,37 @@
 let apiKey = '98a0b814d992d116fe262181b9c7399d';
-let city = document.querySelector('#city-input').value;
-let query = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey;
+let city = '';
+let query = '';
 let currentWeather = document.querySelector('#currentWeather');
 let weatherForecast = document.querySelector('#weatherForecast');
 let searchBtn = document.querySelector('.search-button')
 
 
 
-searchBtn.click(function () {
+$(searchBtn).click(function () {
     city = document.querySelector('#city-input').value;
     console.log(city)
 
+    if (city === ''){
+        return;
+    } else {
+        query = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey;
+        fetch(query).then(function(response){
+            return response.json();
+        }).then(function (data){
+            console.log(data);
+            let latitude = data.coord.lat;
+            let longitude = data.coord.lon;
+            getForcast(latitude, longitude);
+            
+            function displayWeather() {
+                let todayWeather = document.createElement('h2');
+                currentWeather.appendChild(todayWeather);
+                todayWeather.textContent = ''
+            }
+        })
+    }
     
 });
-
-console.log(city);
-
-fetch(query).then(function(response){
-    return response.json();
-}).then(function (data){
-    console.log(data);
-    let latitude = data.coord.lat;
-    let longitude = data.coord.lon;
-    getForcast(latitude, longitude);
-})
-
 
 function getForcast(lat, lon){
     let forcastQuery = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&appid='+apiKey;
@@ -35,6 +42,7 @@ function getForcast(lat, lon){
         console.log(data);
     })
 }
+
 
 
 
